35b1b677eb16c7b9134b5a5224a91c74b8174f51
Peace <peaaig123@gmail.com>
2026-06-20 21:33:08 +0100

    removed unused imports

--- src/main/java/com/FACTor/Digital_Wallet/entity/Customer.java
+++ src/main/java/com/FACTor/Digital_Wallet/entity/Customer.java
@@ -2,14 +2,10 @@ package com.FACTor.Digital_Wallet.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
