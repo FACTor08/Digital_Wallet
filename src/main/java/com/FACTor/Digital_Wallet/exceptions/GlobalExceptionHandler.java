c24c06ea44b3a77d009d6abe3b46e33a3ce71d55
Peace <peaaig123@gmail.com>
2026-06-20 21:42:30 +0100

    Global exception handling for ConstraintViolationException

--- src/main/java/com/FACTor/Digital_Wallet/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/FACTor/Digital_Wallet/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.FACTor.Digital_Wallet.exceptions;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,5 +30,10 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, Object>> existingUserException(ExistingUserException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("Status", 409,
                 "Message", ex.getMessage()));
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, Object>> constraintViolation(ConstraintViolationException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("Status", 400,
+                "Message",ex.getMessage()));
     }
 }
