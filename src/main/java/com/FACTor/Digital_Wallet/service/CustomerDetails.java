7ba7b25fa472bc8ceca25cce94cce476979e4615
Peace <peaaig123@gmail.com>
2026-06-20 21:34:28 +0100

    annotated method loadUserByUsername as Transactional

--- src/main/java/com/FACTor/Digital_Wallet/service/CustomerDetails.java
+++ src/main/java/com/FACTor/Digital_Wallet/service/CustomerDetails.java
@@ -8,12 +8,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerDetails implements UserDetailsService {
     private final CustomerRepo repo;
 
+    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username){
         Customer user = repo.findByUsername(username)     //searches for user in the db
