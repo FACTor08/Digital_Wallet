3347f8ff2d5939b1c1e5c8b30c0bae9a08bcea4e
Peace <peaaig123@gmail.com>
2026-06-28 01:45:55 +0100

    made a separate method to upload profile photo to ensure visibility on swagger ui

--- src/main/java/com/FACTor/Digital_Wallet/service/CustomerLogic.java
+++ src/main/java/com/FACTor/Digital_Wallet/service/CustomerLogic.java
@@ -6,6 +6,7 @@ import com.FACTor.Digital_Wallet.exceptions.ExistingUserException;
 import com.FACTor.Digital_Wallet.mapper.CustomerMapper;
 import com.FACTor.Digital_Wallet.repository.CustomerRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,32 +22,45 @@ public class CustomerLogic {
     private final CustomerRepo repo;
     private final CustomerMapper mapper;
 
-   public String addUser(CustomerDTO data, MultipartFile file) throws IOException {
-Optional<Customer> checkEmail = repo.findByEmail(data.getEmail());
-Optional<Customer> checkBvn = repo.findByBvn(data.getBvn());
-Optional<Customer> checkNin = repo.findByNin(data.getNin());
+    public String addUser(CustomerDTO data){
+        Optional<Customer> checkEmail = repo.findByEmail(data.getEmail());
+        Optional<Customer> checkBvn = repo.findByBvn(data.getBvn());
+        Optional<Customer> checkNin = repo.findByNin(data.getNin());
 
-       boolean bvn, nin, phone;
-       bvn = String.valueOf(data.getBvn()).length() != 11;
-       nin = String.valueOf(data.getNin()).length() != 11;
-       phone = data.getPhone().length() != 11;
-       if (bvn||nin||phone){
-           throw new IllegalArgumentException("Please Confirm your NIN/BVN/Phone-number is correct!");
-       }
-       // throws error code 409 If email,bvn or nin already exists or else creates a new account
+        // makes sure bvn,nin and phone are either valid(11 digits) or not provided
+        boolean invalidBvn, invalidNin, invalidPhone;
+        invalidBvn = data.getBvn() != 0 && String.valueOf(data.getBvn()).length() != 11;
+        invalidNin = data.getNin() != 0 && String.valueOf(data.getNin()).length() != 11;
+        invalidPhone = data.getPhone() != null && data.getPhone().length() != 11;
+        if (invalidBvn || invalidNin || invalidPhone) {
+            throw new IllegalArgumentException("Please Confirm your NIN/BVN/Phone-number is correct!");
+        }
+        // throws error code 409 If email,bvn or nin already exists or else creates a new account
+        if (checkEmail.isPresent() || checkBvn.isPresent() || checkNin.isPresent()) {
+            throw new ExistingUserException("User already Exists!");
+        }
 
-if (checkEmail.isPresent() || checkBvn.isPresent() ||checkNin.isPresent()){
-     throw new ExistingUserException("User already Exists!");
-}
-       byte[] image = file.getBytes();
-       String imageType = file.getContentType();
-       Customer customer = mapper.userTransfr(data, image, imageType);
-
-       repo.save(customer); //save user data to the db
-       if(data.getUsername().isEmpty()){
-       return "Hey " + data.getFirstname() + " your new Wallet is almost ready🤗, Thank you for choosing us❤" ;
-       }else{
-           return "Hey " + data.getUsername() + " your new Wallet is almost ready🤗, Thank you for choosing us❤" ;
-       }
-   }
+        Customer customer = mapper.userTransfer(data);
+        repo.save(customer); //save user data to the db
+        if (data.getUsername().isEmpty()) {
+            return "Hey " + data.getFirstname() + " your new Wallet is almost ready🤗, Thank you for choosing us❤";
+        } else {
+            return "Hey " + data.getUsername() + " your new Wallet is almost ready🤗, Thank you for choosing us❤";
+        }
+    }
+    //A method for user to upload profile picture
+    public String addUserImage(String user, MultipartFile file) throws IOException {
+        Optional<Customer> username = repo.findByUsername(user);
+        Optional<Customer> firstname  = repo.findByFirstname(user);
+        if (username.isEmpty() && firstname.isEmpty()) {
+            throw new UsernameNotFoundException("User Not Found");
+        }
+        Customer update = username.get();
+        byte[] image = file.getBytes();
+        String imageType = file.getContentType();
+        update.setImage(image);
+        update.setImageType(imageType);
+        repo.save(update);
+        return "Profile picture successfully added";
+    }
 }
