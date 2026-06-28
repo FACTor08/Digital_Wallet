6e5263430a7de6efbd96ff3dcdf98a08186e9b78
Peace <peaaig123@gmail.com>
2026-06-28 01:41:59 +0100

    removed unused imports

--- src/main/java/com/FACTor/Digital_Wallet/dto/CustomerDTO.java
+++ src/main/java/com/FACTor/Digital_Wallet/dto/CustomerDTO.java
@@ -1,9 +1,6 @@
 package com.FACTor.Digital_Wallet.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
