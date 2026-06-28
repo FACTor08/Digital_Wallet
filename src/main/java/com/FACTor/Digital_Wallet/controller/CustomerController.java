4248bacf8cf043899f221e56d833d41673dec6ce
Peace <peaaig123@gmail.com>
2026-06-28 01:35:01 +0100

    added profile picture upload controller endpoint

--- src/main/java/com/FACTor/Digital_Wallet/controller/CustomerController.java
+++ src/main/java/com/FACTor/Digital_Wallet/controller/CustomerController.java
@@ -3,13 +3,9 @@ package com.FACTor.Digital_Wallet.controller;
 import com.FACTor.Digital_Wallet.dto.CustomerDTO;
 import com.FACTor.Digital_Wallet.service.CustomerLogic;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -19,10 +15,17 @@ import java.io.IOException;
 public class CustomerController {
     private final CustomerLogic logic;
 
-    @PostMapping(value = "/user-signup", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> addUser(@ModelAttribute CustomerDTO data,
-                                          @RequestPart(name = "image") MultipartFile image) throws IOException {
-        String msg = logic.addUser(data, image);
+    @PostMapping("/user-signup")
+    public ResponseEntity<String> addUser(@RequestBody CustomerDTO data) {
+        String msg = logic.addUser(data);
         return ResponseEntity.ok(msg);
     }
+
+    @PostMapping(value = "/profile-picture-upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadProfile(@ModelAttribute String user,
+                                                @RequestPart(name = "image") MultipartFile image) throws IOException {
+
+        String msg = logic.addUserImage(user, image);
+        return ResponseEntity.ok(msg);
+    }   
 }
