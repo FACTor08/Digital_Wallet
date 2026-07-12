304e78fdf264ae2f279a833416e92a3b36359103
Peace <peaaig123@gmail.com>
2026-07-12 01:56:48 +0100

    fix: "ResourceNotFoundException" class to "AccountNotFound"

--- src/main/java/com/FACTor/Digital_Wallet/exceptions/AccountNotFoundException.java
+++ src/main/java/com/FACTor/Digital_Wallet/exceptions/AccountNotFoundException.java
@@ -1,8 +1,8 @@
 package com.FACTor.Digital_Wallet.exceptions;
 
 //throws 404 not found error status code
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
+public class AccountNotFoundException extends RuntimeException{
+    public AccountNotFoundException(String message){
         super(message);
     }
 }
